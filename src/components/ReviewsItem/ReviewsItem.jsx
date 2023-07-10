bec0f97552f32af52c5a3622681953deb702a0f5
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-07-10 22:15:24 +0200

    update

--- src/components/ReviewsItem/ReviewsItem.jsx
+++ src/components/ReviewsItem/ReviewsItem.jsx
@@ -2,10 +2,10 @@ import { PropTypes } from 'prop-types';
 
 const ReviewsItem = ({ author, content }) => {
   return (
-    <>
+    <li>
       <h3>Author: {author}</h3>
       <p>{content}</p>
-    </>
+    </li>
   );
 };
 
